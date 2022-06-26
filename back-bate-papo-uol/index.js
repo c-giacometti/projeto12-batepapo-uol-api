c09bdddcb4e2bedeb5e0695aea1ed7a0ff7849da
c.giacometti <camilla.giacometti@gmail.com>
2022-06-26 20:42:17 -0300

    rota status e remover participantes inativos

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(cors());
 const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
 
+const QUINZE_SEGUNDOS = 15*1000;
+const DEZ_SEGUNDOS = 10;
+
 app.post("/participants", async (req, res) => {
 
     let nome = req.body.name;
@@ -87,8 +90,8 @@ app.post("/messages", async (req, res) => {
         const mensagensCollection = uoldb.collection("mensagens");
         
         const { to, text, type } = req.body;
+        const usuario = req.headers.User;
         const horaEnvio = dayjs().format("hh:mm:ss");
-        const usuario = req.headers.user;
 
         const mensagemPost = {
             from: usuario,
@@ -99,7 +102,7 @@ app.post("/messages", async (req, res) => {
         }
 
         mensagensCollection.insertOne(mensagemPost);
-        res.status(201).send(mensagemPost);
+        res.sendStatus(201);
 
         return;
 
@@ -140,7 +143,72 @@ app.get("/messages", async (req, res) => {
         return;
     }
 
-})
+});
 
+app.post("/status", async (req, res) => {
+
+    try {
+        await mongoClient.connect();
+
+        const uoldb = mongoClient.db("uol");
+        const usuariosCollection = uoldb.collection("users");
+
+        const usuario = req.headers.user;
+
+        const usuarioCadastrado = await usuariosCollection.findOne({ name: usuario });
+
+        if(usuarioCadastrado){
+            await usuariosCollection.updateOne(
+                { lastStatus: usuarioCadastrado.lastStatus }, 
+                { $set: {lastStatus: Date.now()} }
+            );
+            res.sendStatus(200);
+            return;
+        } else {
+            res.sendStatus(404);
+            return;
+        }
+
+    }
+
+    catch(error){
+        res.sendStatus(500);
+        mongoClient.close();
+        return;
+    }
+
+});
+
+async function removerParticipantes(){
+    await mongoClient.connect();
+
+    const uoldb = mongoClient.db("uol");
+    const usuariosCollection = uoldb.collection("users");
+    const mensagensCollection = uoldb.collection("mensagens");
+
+    const participantesAtivos = await usuariosCollection.find({}).toArray();
+
+    participantesAtivos.forEach((item) => {
+        const tempoInativo = Date.now() - item.lastStatus;
+
+        if(tempoInativo > DEZ_SEGUNDOS) {
+
+            const horaSaida = dayjs().format("hh:mm:ss");
+
+            mensagensCollection.insertOne({
+                from: item.name,
+                to: "Todos",
+                text: "sai da sala...",
+                type: "status",
+                time: horaSaida
+            });
+
+            usuariosCollection.deleteOne({ name: item.name });
+        }
+        
+    });
+}
+
+setInterval(removerParticipantes, QUINZE_SEGUNDOS);
 
 app.listen(5000);
